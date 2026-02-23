c148e36ada52f197268027148fc5594e0605ffae
Thien phuc <thienphuc484@gmail.com>
2026-02-23 09:46:26 +0700

    Add Role to UpdateUserDto

--- Application/Dtos/Request/UpdateUserDtos.cs
+++ Application/Dtos/Request/UpdateUserDtos.cs
@@ -1,3 +1,3 @@
 namespace Application.Dtos.Request;
 
-public record UpdateUserDto(string Username, string Email, string Password);
+public record UpdateUserDto(string? Username, string? Email, string? Password, string? Role);
