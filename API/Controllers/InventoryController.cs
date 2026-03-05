ff5896eca815e9e5d453986d70c8ced9c5255982
Thien phuc <thienphuc484@gmail.com>
2026-03-05 09:03:35 +0700

    Add Authorize to update() and delete() in InvetoryController

--- API/Controllers/InventoryController.cs
+++ API/Controllers/InventoryController.cs
@@ -13,29 +13,38 @@ public class InventoryController(IInventoryService service) : ControllerBase
 {
 
     [HttpGet, ProducesResponseType(typeof(Inventory), 200), ProducesResponseType(500)]
-    public async Task<IActionResult> FindAll() => Ok(await service.FindAll());
+    public async Task<IActionResult> FindAll()
+    {
+        return Ok(await service.FindAll());
+    }
 
     [HttpPost, ProducesResponseType(typeof(Inventory), 201), ProducesResponseType(401), ProducesResponseType(500)]
     public async Task<IActionResult> Create([FromBody] InventoryDto inventoryDto)
     {
-        var result = await service.Create(inventoryDto);
+        Inventory result = await service.Create(inventoryDto);
         return CreatedAtAction(nameof(FindById), new { id = result.Id }, result);
     }
 
     [HttpPut("{id:int}"), ProducesResponseType(204), ProducesResponseType(400), ProducesResponseType(404), ProducesResponseType(500)]
+    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Manager")]
     public async Task<IActionResult> Update([FromBody] InventoryDto inventoryDto, int id)
     {
-        await service.Update(id, inventoryDto);
+        _ = await service.Update(id, inventoryDto);
         return NoContent();
     }
 
     [HttpGet("{id:int}"), ProducesResponseType(typeof(Inventory), 200), ProducesResponseType(404), ProducesResponseType(500)]
-    public async Task<IActionResult> FindById(int id) => Ok(await service.FindById(id));
+    public async Task<IActionResult> FindById(int id)
+    {
+        return Ok(await service.FindById(id));
+    }
 
     [HttpDelete("{id:int}"), ProducesResponseType(204), ProducesResponseType(404), ProducesResponseType(500)]
+    [Authorize(Roles = "Admin")]
     public async Task<IActionResult> Delete(int id)
     {
-        await service.Delete(id);
+        _ = await service.Delete(id);
         return NoContent();
     }
 }
